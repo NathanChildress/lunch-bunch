2c9b756758b4f592657f126c47f36417fb764039
Alejandra Patino <ale@alejandras-mbp.home>
2020-08-25 11:20:29 -0700

    added GuestForm component

---
+++ src/components/AddGuestsForm/AddGuestsForm.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const AddGuestsForm = (props) => {
+    return (
+        <form>
+            <label>Name:</label>
+            <input type="text" name="name" value="" />
+            <br/>
+            <label>Address:</label>
+            <input type="text" name="address" value="" />
+            <br/>
+            <button>Add new guest</button>
+          
+          </form>
+    )
+}
+
+
+export default AddGuestsForm;
