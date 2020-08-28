3d2da6eb26599c3dd396c2736882683667ae625d
Alejandra Patino <ale@alejandras-mbp.home>
2020-08-27 19:00:20 -0700

    styled sign up page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -37,41 +37,21 @@ class LoginPage extends Component {
   render() {
     return (
       <>
-<form class="login-form" onSubmit={this.handleSubmit}>
-  <p class="login-text">
-    <span>
-      <i><img src="https://i.imgur.com/2OB3dfx.png" alt=""/></i>
-    </span>
-  </p>
-  <input type="email" className="login-username" autofocus="true" required="true" placeholder="Email" name="email" value={this.state.email} onChange={this.handleChange} />
-  <input type="password" className="login-password" required="true" placeholder="Password" value={this.state.pw} onChange={this.handleChange} name="pw"/>
-  <input type="submit" name="Login" value="Login" className="login-submit" />
-</form>
-</>
+        <form class="login-form" onSubmit={this.handleSubmit}>
+          <header className="login-header">Lunch Bunch</header>
+            <p class="login-text">
+          <span>
+            <i><img src="https://i.imgur.com/2OB3dfx.png" alt=""/></i>
+          </span>
+            </p>
+          <input type="email" className="login-username" autofocus="true" required="true" placeholder="Email" name="email" value={this.state.email} onChange={this.handleChange} />
+          <input type="password" className="login-password" required="true" placeholder="Password" value={this.state.pw} onChange={this.handleChange} name="pw"/>
+          <input type="submit" name="Login" value="Login" className="login-submit" />
+          </form>
+      </>
     );
   }
 }
 
 export default LoginPage;
 
-{/* <div className="LoginPage">
-<header className="header-footer">Log In &nbsp; 🍔 </header>
-<form className="form-horizontal" onSubmit={this.handleSubmit} >
-  <div className="form-group">
-    <div className="col-sm-12">
-      <input type="email" className="form-control" placeholder="Email" value={this.state.email} name="email" onChange={this.handleChange} />
-    </div>
-  </div>
-  <div className="form-group">
-    <div className="col-sm-12">
-      <input type="password" className="form-control" placeholder="Password" value={this.state.pw} name="pw" onChange={this.handleChange} />
-    </div>
-  </div>
-  <div className="form-group">
-    <div className="col-sm-12 text-center">
-      <button className="btn btn-default">Log In</button>&nbsp;&nbsp;&nbsp;
-      <button><Link to='/'>Cancel</Link></button>
-    </div>
-  </div>
-</form>
-</div> */}
